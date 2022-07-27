13e42c68b36f21fffaaebb317f8f28019efcc990
raulgarciawk <109366363+raulgarciawk@users.noreply.github.com>
2022-07-27 13:38:48 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,13 @@ module.exports = {
           css: {
             pre: {
               padding: "0",
-              color: "#1F2933",
+              //color: "#1F2933",
               backgroundColor: "#F3F3F3"
             },
             code: {
               padding: "0.2em 0.4em",
-              backgroundColor: "#F3F3F3",
-              color: "#DD1144",
+              //backgroundColor: "#F3F3F3",
+              //color: "#DD1144",
               fontWeight: "400",
               "border-radius": "0.25rem"
             },
